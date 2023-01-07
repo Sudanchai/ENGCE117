c3043998692a10ce236966530bf69f75eadcc8aa
Sudanchai <118188538+Sudanchai@users.noreply.github.com>
2023-01-07 16:28:06 +0700

    Update code5-2.c

--- Practise1/Lap05/code5-2.c
+++ Practise1/Lap05/code5-2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #include <string.h>
 
 struct studentNode {
@@ -9,61 +10,60 @@ struct studentNode {
   struct studentNode * next;
 };
 
-void ShowAll(struct studentNode ** walk);
-struct studentNode * AddNode(struct studentNode ** head, char n[], int a, char s, float g);
-void InsNode(struct studentNode * prev, char n[], int a, char s, float g);
-void DelNode(struct studentNode * prev);
+void ShowAll(struct studentNode ** walk) {
+  while ( * walk != NULL) {
+    printf("%s ", ( * walk) -> name);
+    walk = & ( * walk) -> next;
+  } //end while
+  printf(" ");
+} //end function
+
+struct studentNode * AddNode(struct studentNode ** head, char n[], int a, char s, float g) {
+  struct studentNode * newNode = (struct studentNode * ) malloc(sizeof(struct studentNode));
+  strcpy(newNode -> name, n);
+  newNode -> age = a;
+  newNode -> sex = s;
+  newNode -> gpa = g;
+  newNode -> next = * head;
+  * head = newNode;
+  return newNode;
+} //end function
+
+void InsNode(struct studentNode ** prev, char n[], int a, char s, float g) {
+  struct studentNode * newNode = (struct studentNode * ) malloc(sizeof(struct studentNode));
+  strcpy(newNode -> name, n);
+  newNode -> age = a;
+  newNode -> sex = s;
+  newNode -> gpa = g;
+  newNode -> next = ( * prev) -> next;
+  ( * prev) -> next = newNode;
+} //end function
+
+void DelNode(struct studentNode ** prev) {
+  struct studentNode * temp = ( * prev) -> next;
+  * prev = temp -> next;
+  free(temp);
+} //end function
+
+void GoNext(struct studentNode ** ptr) {
+  * ptr = ( * ptr) -> next;
+} //end function
 
 int main() {
   struct studentNode * start, ** now;
   start = NULL;
   now = & start;
   InsNode(now, "one", 6, 'M', 3.11);
-  ShowAll(&start);
+  ShowAll( & now);
   InsNode(now, "two", 8, 'F', 3.22);
-  ShowAll(&start);
-  now = &((*now)->next);
+  ShowAll( & now);
+  GoNext( & now);
   InsNode(now, "three", 10, 'M', 3.33);
-  ShowAll(&start);
+  ShowAll( & now);
   InsNode(now, "four", 12, 'F', 3.44);
-  ShowAll(&start);
-  now = &((*now)->next);
+  ShowAll( & now);
+  GoNext( & now);
   DelNode(now);
-  ShowAll(&start);
+  ShowAll( & now);
   return 0;
-} //end function
-
-void ShowAll(struct studentNode ** walk) {
-    while (*walk != NULL) {
-      printf("%s ", (*walk)->name);
-      walk = &(*walk)->next;
-    } //end while
-    printf("\n");
-} //end function
-
-struct studentNode * AddNode(struct studentNode ** head, char n[], int a, char s, float g) {
-  struct studentNode * newNode = (struct studentNode*)malloc(sizeof(struct studentNode));
-  strcpy(newNode->name, n);
-  newNode->age = a;
-  newNode->sex = s;
-  newNode->gpa = g;
-  newNode->next = *head;
-  *head = newNode;
-  return newNode;
-} //end function
-
-void InsNode(struct studentNode * prev, char n[], int a, char s, float g) {
-  struct studentNode * newNode = (struct studentNode*)malloc(sizeof(struct studentNode));
-  strcpy(newNode->name, n);
-  newNode->age = a;
-  newNode->sex = s;
-  newNode->gpa = g;
-  newNode->next = prev->next;
-  prev->next = newNode;
-} //end function
-
-void DelNode(struct studentNode * prev) {
-  struct studentNode * temp = prev->next;
-  prev->next = temp->next;
-  free(temp);
 } //end function
